bda58975613e69d8f1a18b7b0c68953f58586283
Lucian Cristian <cristianlucian84@gmail.com>
2019-04-15 21:52:02 +0300

    add person to db

--- main/views.py
+++ main/views.py
@@ -11,3 +11,44 @@ def formular(request):
 def formular_submit(request):
     return render(request, "main/raspunsformular.html",{'name':request.POST['nume'],'prenume':request.POST['prenume'],'trimite':request.POST['trimite']})
 # Create your views here.
+
+
+
+
+
+#
+# def person(request):
+#     if request.method == 'POST':
+#         form = PersonForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             print(cd['nume'])
+#             print(cd['email'])
+#             print(cd['phonenumber'])
+#         else:
+#             print("Form is not valid")
+#     else:
+#         form = PersonForm()
+#     return render(request, 'main/contact_form.html', {'form': form})
+
+
+from .forms import PersonForm
+from .models import Person
+
+def person(request):
+    if request.method == 'POST':
+        form = PersonForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            person = Person()
+            person.nume = cd['nume']
+            person.mail = cd['email']
+            person.phonenumber = cd['phonenumber']
+            person.save()
+            form = PersonForm()
+        else:
+            print("Form is not valid")
+    else:
+        form = PersonForm()
+
+    return render(request, 'main/contact_form.html', {'form': form})
