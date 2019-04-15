48f2b321fdab6c796fd395ef8079134a26029731
Lucian Cristian <cristianlucian84@gmail.com>
2019-04-15 20:03:09 +0300

    add template hello world

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, World")
+    return render(request, "main/home.html")
 # Create your views here.
