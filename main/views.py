1004186beae1235052286ceed0bc5360df85bdca
Lucian Cristian <cristianlucian84@gmail.com>
2019-04-15 21:16:58 +0300

    added formular

--- main/views.py
+++ main/views.py
@@ -5,3 +5,9 @@ from django.http import HttpResponse
 def home(request):
     return render(request, "main/home.html", {'message': 'Hi, there!'})
 # Create your views here.
+def formular(request):
+    return render(request, "main/formular.html")
+
+def formular_submit(request):
+    return render(request, "main/raspunsformular.html",{'name':request.POST['nume'],'prenume':request.POST['prenume'],'trimite':request.POST['trimite']})
+# Create your views here.
