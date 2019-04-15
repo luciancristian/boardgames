70ca1ffe7c2cdddc407ebcb0da1032f8ab10c2b0
Lucian Cristian <cristianlucian84@gmail.com>
2019-04-15 20:13:37 +0300

    add parameter into view

--- main/views.py
+++ main/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html", {'message': 'Hi, there!'})
 # Create your views here.
